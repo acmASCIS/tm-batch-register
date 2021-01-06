c6c62598d581cbd43de2cffb52b247c714afd18d
Tassneem Hamdy <tassneemhamdy@gmail.com>
2021-01-06 21:43:28 +0200

    feat: server implemintation [WIP]

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('running on 3000');
+});
