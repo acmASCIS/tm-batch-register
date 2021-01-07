c18c8579395223538458e5761c0e3cc6ac0037de
Tassneem Hamdy <tassneemhamdy@gmail.com>
2021-01-07 23:07:29 +0200

    feat: server implementation

--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+async function handleSubmit() {
+  const mentorHandle = document.getElementById('mentorHandleInput').value;
+  const traineeData = document.getElementById('traineesDataInput').value;
+  const trainees = traineeData.split('\n').map((x) => {
+    const [name, email, codeforcesHandle] = x.split('\t');
+    return {
+      name: name.trim(),
+      email: email.trim(),
+      codeforcesHandle: codeforcesHandle.trim(),
+    };
+  });
+
+  try {
+    await axios.post('/register', trainees);
+    alert('Success');
+  } catch {
+    alert('Error');
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
+const { default: axios } = require('axios');
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+const API_URL = process.env.API_URL || 'https://trainees-monitor-dev.herokuapp.com/register';
+const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: process.env.ORIGIN,
+}));
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('running on 3000');
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+  try {
+    const promises = req.body.map((element) => axios.post(API_URL, {
+      ...element,
+      handle: element.codeforcesHandle,
+      password: '123456',
+      confirmPassword: '123456',
+      role: 0,
+    }));
+    await Promise.all(promises);
+    res.status(200).json({ error: false });
+  } catch (error) {
+    res.status(500).json({ error: true });
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`running on ${port}`);
 });
